1f62d41759045783a21b3e4ecc13961b5e556a18
BreezyCoder <62312009+BreezyCoder@users.noreply.github.com>
2021-02-18 11:30:12 +0200

    Update users.js

--- users.js
+++ users.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post("/", function (req, res, next) {
+  //create each individual cosnt for POST data coming from the web app 
+  //capture it and store them in const's
   const name = req.body.name;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const password = req.body.password;
   const checkBox = req.body.checkBox;
+  //check if the POST data has been interpreted correctly and log them to the console 
   console.log(
     name + " " + lastName + " " + email + " " + password + " " + checkBox
   );
